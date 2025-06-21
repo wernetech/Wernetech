9bd5391a07c055ab5284fb12300fc329ba6b3296
Davi Quaresma da Silva Henriques <102613671+DaviQuaresma@users.noreply.github.com>
2025-06-21 17:56:28 -0300

    Correções no serviço de email

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -2,14 +2,16 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: true,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: process.env.SMTP_SECURE === "true",
+  requireTLS: process.env.SMTP_REQUIRE_TLS === "true",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
 });
+
 
 export async function sendEmail({ to, subject, html }) {
     try {
